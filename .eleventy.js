e6ddb94d1a1c122f83d39b125e80342919eced7d
BonacinaAndrea <bonacinaandrea90@gmail.com>
2026-04-10 10:29:47 +0200

    Add collections for servizi, testimonianze, and casi_studio

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,18 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("admin");
   eleventyConfig.addPassthroughCopy("api");
 
+  eleventyConfig.addCollection("servizi", function(collectionApi) {
+    return collectionApi.getFilteredByGlob("_servizi/*.md");
+  });
+
+  eleventyConfig.addCollection("testimonianze", function(collectionApi) {
+    return collectionApi.getFilteredByGlob("_testimonianze/*.md");
+  });
+
+  eleventyConfig.addCollection("casi_studio", function(collectionApi) {
+    return collectionApi.getFilteredByGlob("_casi_studio/*.md");
+  });
+
   return {
     dir: {
       input: "src",
