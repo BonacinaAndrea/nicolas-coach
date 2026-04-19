980b8131ec256cdda8968994e3c805479233c220
BonacinaAndrea <bonacinaandrea90@gmail.com>
2026-04-19 17:25:24 +0200

    Implement function to read and parse markdown files
    Add function to read markdown files and extract front matter data.

--- _data/casi_studio.js
+++ _data/casi_studio.js
@@ -1,1 +1,11 @@
+const fs = require('fs');
+const path = require('path');
+const matter = require('gray-matter');
 
+module.exports = function() {
+  const dir = path.join(__dirname, '../src/_casi_studio');
+  if (!fs.existsSync(dir)) return [];
+  return fs.readdirSync(dir)
+    .filter(f => f.endsWith('.md'))
+    .map(f => matter(fs.readFileSync(path.join(dir, f), 'utf8')).data);
+};
